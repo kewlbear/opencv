cba70036d26ba0f46076b8f90ff4998d9fb0c219
Changbeom Ahn <kewlbear@gmail.com>
2021-07-13 14:30:25 +0900

    add opencv2 target to library product

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "opencv",
-            targets: ["opencv"]),
+            targets: ["opencv2", "opencv"]),
     ],
     targets: [
         .binaryTarget(name: "opencv2", url: "https://github.com/kewlbear/opencv/releases/download/0.0.1/opencv2.xcframework.zip", checksum: "2d2a52e4e5512ff6c4265c82819ad2b460e0d5774974fd87f02fbe8d238535c0"),
